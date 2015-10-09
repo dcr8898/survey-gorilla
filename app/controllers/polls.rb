226156cd83e2df8d2ff36e4220c8dd1817c3985d
Phil Sofia <phil.sofia@gmail.com>
2015-10-09 18:34:43 -0400

    Update polls routes

--- app/controllers/polls.rb
+++ app/controllers/polls.rb
@@ -4,12 +4,20 @@ get '/polls' do
 end
 
 get '/polls/:id' do
-  @poll = Poll.new(user_id: session[:id], survey_id: params[:id])
+  # Change user_id to session[:id] once it is implemented
+  @poll = Poll.create(user_id: 14, survey_id: params[:id])
   @survey = Survey.find(params[:id])
   erb :'polls/show'
 end
 
 post '/polls' do
-
+  poll = params[:response][:poll_id]
+  choices = params[:response][:choices]
+  @surveys = Survey.all
+  # This is the poll id: 102 This is what is in choices: {"1"=>"4", "2"=>"7"}
+  choices.values.each do |choice|
+    Response.create(poll_id: poll, choice_id: choice)
+  end
+  erb :'polls/complete'
 
 end
