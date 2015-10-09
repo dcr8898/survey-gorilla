c3ff564047e48cf11f8bb9b903bcb199fe5e6c92
Phil Sofia <phil.sofia@gmail.com>
2015-10-09 17:33:13 -0400

    Update seed file so survey has two questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ end
 survey = Survey.create(user_id: User.first.id,
                        name:    "DBC Feedback")
 
-question = Question.create(survey_id: survey.id,
+question1 = Question.create(survey_id: survey.id,
                            text:      "How much do you love DBC?")
 
 choices = ["A lot",
@@ -17,9 +17,23 @@ choices = ["A lot",
            "Around the moon and back again"]
 
 choices.each do |choice|
-  Choice.create(question_id: question.id,
+  Choice.create(question_id: question1.id,
                 text: choice)
 end
+
+question2 = Question.create(survey_id: survey.id,
+                           text:      "How awesome is this?")
+
+choices = ["A lot",
+           "A whole lot",
+           "A super-duper-lot",
+           "Around the moon and back again"]
+
+choices.each do |choice|
+  Choice.create(question_id: question2.id,
+                text: choice)
+end
+
 
 100.times do |i|
   poll = Poll.create(user_id:    User.ids.sample,
