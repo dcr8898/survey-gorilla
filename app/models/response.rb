c92815325ca49a6298342afbc0199ef87daeabfc
Phil Sofia <phil.sofia@gmail.com>
2015-10-10 14:36:17 -0400

    Add methods to poll and reponse models to move logic from views to models

--- app/models/response.rb
+++ app/models/response.rb
@@ -4,4 +4,16 @@ class Response < ActiveRecord::Base
 
   validates :poll_id, presence: true
   validates :choice_id, presence: true
+
+  def question
+    self.choice.question.text
+  end
+
+  def text
+    self.choice.text
+  end
+
+  def question_choices
+    self.choice.question.choices
+  end
 end
