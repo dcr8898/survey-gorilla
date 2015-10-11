e1148ab3ffe57e7e7359badc05140293ee47b87b
Phil Sofia <phil.sofia@gmail.com>
2015-10-10 23:23:52 -0400

    Edit user controller so that login and signup immediately create a new session

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -18,7 +18,7 @@ post '/login' do
     @errors = ["Cannot find user #{params[:username]}."]
     erb :'users/login'
   elsif @user.password == params[:password]
-    session[:user_id] == @user.id
+    session[:user_id] = @user.id
     redirect '/'
   else
     @errors = ["Incorrect password."]
@@ -35,6 +35,7 @@ post '/users' do
   @user = User.new(params[:user])
 
   if @user.save
+    session[:user_id] = @user.id
     redirect '/'
   else
     @errors = @user.errors.full_messages
