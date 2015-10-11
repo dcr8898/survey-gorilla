95832b7098d2ce180315d24cedfb14cbc8bc0dc6
Phil Sofia <phil.sofia@gmail.com>
2015-10-10 21:52:37 -0400

    Add two associations to response model: question and choices

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,19 +1,13 @@
 class Response < ActiveRecord::Base
   belongs_to :poll
   belongs_to :choice
+  has_one :question, :through => :choice
+  has_many :choices, :through => :question
 
   validates :poll_id, presence: true
   validates :choice_id, presence: true
 
-  def question
-    self.choice.question.text
-  end
-
   def text
     self.choice.text
-  end
-
-  def question_choices
-    self.choice.question.choices
   end
 end
