d3516aa90a57dfe1b84ecc5c8b3a588465b9559b
Damian C. Rossney <dcrossney@gmail.com>
2015-10-11 13:58:21 -0400

    Add POST /questions/:question_id/choices route
    Add route to accept new choice form input.
    Corrected choices/new view to show question text.

--- app/controllers/choices.rb
+++ app/controllers/choices.rb
@@ -3,3 +3,15 @@ get '/questions/:question_id/choices/new' do
   @choice = Choice.new
   erb :'choices/new'
 end
+
+post '/questions/:question_id/choices' do
+  @question = Question.find_by(id: params[:question_id])
+  @choice = Choice.new(params[:choice])
+  @choice.question_id = @question.id
+  if @choice.save
+    redirect "/questions/#{@question.id}/choices/new"
+  else
+    @errors = @choice.errors.full_messages
+    erb :'choices/new'
+  end
+end
