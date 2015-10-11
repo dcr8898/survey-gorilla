ac182fb53aac58599af13898cb4983607acd51b5
Damian C. Rossney <dcrossney@gmail.com>
2015-10-11 09:51:39 -0400

    Add POST /surveys route
    Add all routes necessary to create a new survey, with views and error messages.
    Created _form_errors partial for use throughout app.

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -3,12 +3,23 @@ get '/surveys' do
   erb :'surveys/index'
 end
 
-get '/surveys/:survey_id' do
-  @survey = Survey.find_by(id: params[:survey_id])
-  erb :'surveys/show'
+post '/surveys' do
+  @survey = Survey.new(params[:survey])
+  @survey.user_id = current_user.id
+  if @survey.save
+    redirect "/surveys/#{@survey.id}/questions/new"
+  else
+    @errors = @survey.errors.full_messages
+    erb :'surveys/new'
+  end
 end
 
 get '/surveys/new' do
   @survey = Survey.new
   erb :'surveys/new'
+end
+
+get '/surveys/:survey_id' do
+  @survey = Survey.find_by(id: params[:survey_id])
+  erb :'surveys/show'
 end
