f48d66170e88d9293bc75c36a5e3713fc1c07037
Damian C. Rossney <dcrossney@gmail.com>
2015-10-11 11:57:54 -0400

    Add POST /surveys/:survey_id/questions route
    Add route to create new question based on form submission
    Refactored related form to display validation errors

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -8,3 +8,16 @@ get '/surveys/:survey_id/questions/new' do
   @question = Question.new
   erb :'questions/new'
 end
+
+post '/surveys/:survey_id/questions' do
+  @survey = Survey.find_by(id: params[:survey_id])
+  @question = Question.new(params[:question])
+  @question.survey_id = @survey.id
+  if @question.save
+    redirect "/surveys/#{@question.survey.id}/questions/#{@question.id}/choices/new"
+  else
+    @errors = @question.errors.full_messages
+    erb :'questions/new'
+  end
+end
+
