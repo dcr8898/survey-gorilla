88f56b5a8c3d8d33510677219cd47930f18c47b0
Damian C. Rossney <dcrossney@gmail.com>
2015-10-11 11:44:06 -0400

    Add GET /surveys/:survey_id/questions/new route
    Add route with form for new question

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -2,3 +2,9 @@ get '/surveys/:survey_id/questions' do
   @survey = Survey.find_by(id: params[:survey_id])
   erb :'surveys/questions'
 end
+
+get '/surveys/:survey_id/questions/new' do
+  @survey = Survey.find_by(id: params[:survey_id])
+  @question = Question.new
+  erb :'questions/new'
+end
